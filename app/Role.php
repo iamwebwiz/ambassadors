187e7609810c8f6e584fb541dd4c903990747af3
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 16:52:45 +0100

    Role model modified

--- app/Role.php
+++ app/Role.php
@@ -7,5 +7,8 @@ use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
-    //
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'role_user', 'role_id', 'user_id');
+    }
 }
