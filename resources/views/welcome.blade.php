9652437b4ff1c0892cff14fa1fc1cfe99af865c5
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 13:45:02 +0100

    Welcome view modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
-        <title>Timber</title>
+        <title>Digital Ambassadors &mdash; Soft Work</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
@@ -60,13 +59,14 @@
                     </div>
                 </div>
                 <div class="col-md-8">
-                    <div class="navmenu"style="text-align: center;">
+                    <div class="navmenu" style="text-align: center;">
                         <ul id="menu">
                             <li class="active" ><a href="#home">Home</a></li>
                             <li><a href="#about">About</a></li>
                             <li><a href="#project">Projects</a></li>
                             <li><a href="#news">News</a></li>
-                            <li class="last"><a href="#contact">Contact</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                            <li class="last"><a href="{{ url('register') }}">Signup</a></li>
                             <!--li><a href="#features">Features</a></li-->
                         </ul>
                     </div>
