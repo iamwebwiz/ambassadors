a46b170949d89e1e0512cade30002aef027ffe0b
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 15:06:45 +0100

    helper file modified

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -5,20 +5,20 @@ use App\User;
 
 if (!function_exists("createRoles")) {
     function createRoles($withAdmin = "yes") {
-        if (Role::all()->count() < 1) {
-            $publisher = new Role;
+        if (count(Role::all()) < 1) {
+            $publisher = new Role();
             $publisher->name = "publisher";
             $publisher->display_name = "Publisher";
             $publisher->description = "Publisher post adverts for business owners";
             $publisher->save();
 
-            $client = new Role;
+            $client = new Role();
             $client->name = "client";
             $client->display_name = "Business Owner";
             $client->description = "Business owner brings their works to be advertised";
             $client->save();
 
-            $admin = new Role;
+            $admin = new Role();
             $admin->name = "admin";
             $admin->display_name = "Administrator";
             $admin->description = "Administrator oversees the activities on the platform";
