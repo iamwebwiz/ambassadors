243c527011f2d83b3c471848293555f4d9a8a65d
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 16:31:07 +0100

    Modifications

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,38 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Role;
-use App\User;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
-    public function welcome() {
+    public function welcome()
+    {
         if (Role::all()->count() < 1) {
-            $publisher = new Role;
-            $publisher->name = "publisher";
-            $publisher->display_name = "Publisher";
-            $publisher->description = "Publisher post adverts for business owners";
-            $publisher->save();
-
-            $client = new Role;
-            $client->name = "client";
-            $client->display_name = "Business Owner";
-            $client->description = "Business owner brings their works to be advertised";
-            $client->save();
-
-            $admin = new Role;
+            // create new roles
+            $admin = new Role();
             $admin->name = "admin";
             $admin->display_name = "Administrator";
-            $admin->description = "Administrator oversees the activities on the platform";
+            $admin->description = "Admin oversees the whole platform activities";
             $admin->save();
 
-            $dummy = User::create([
-                'name' => 'Admin',
-                'email' => 'admin@dgambassadors.com',
-                'password' => bcrypt('control')]);
+            $publisher = new Role();
+            $publisher->name = "publisher";
+            $publisher->display_name = "Publisher";
+            $publisher->description = "Publisher advertises for business owners";
+            $publisher->save();
 
-            $dummy->attachRole($admin);
+            $client = new Role();
+            $client->name = "client";
+            $client->display_name = "Business Owner";
+            $client->description = "Business Owners seek to advertise their brands";
+            $client->save();
         }
-        return view('welcome');
+        // return view('welcome');
     }
 }
