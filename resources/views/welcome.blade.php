6d9ee8abd42a2ca7cc9cf4b8a941cdb255662dad
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 12:29:48 +0100

    OpenGraph data included

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,23 +2,23 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bell Bootstrap 4 Theme</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>Digital Ambassadors</title>
+    <meta content="width=devie-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
 
     <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
-    <meta property="og:title" content="">
+    <meta property="og:title" content="Digital Ambassadors">
     <meta property="og:image" content="">
-    <meta property="og:url" content="">
-    <meta property="og:site_name" content="">
-    <meta property="og:description" content="">
+    <meta property="og:url" content="http://dgambassadors.com">
+    <meta property="og:site_name" content="Digital Ambassadors">
+    <meta property="og:description" content="Soft Work Is Our Work">
 
     <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
     <meta name="twitter:card" content="summary">
-    <meta name="twitter:site" content="">
-    <meta name="twitter:title" content="">
-    <meta name="twitter:description" content="">
+    <meta name="twitter:site" content="http://dgambassadors.com">
+    <meta name="twitter:title" content="Digital Ambassadors">
+    <meta name="twitter:description" content="Soft Work Is Our Work">
     <meta name="twitter:image" content="">
 
     <!-- Favicon -->
