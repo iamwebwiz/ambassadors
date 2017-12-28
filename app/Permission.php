a2f2225cdf40850eff1fcb0fe289d4e705363144
iamwebwiz <iamwebwiz@gmail.com>
2017-12-28 15:26:04 +0100

    Updated Role and Permission models

--- app/Permission.php
+++ app/Permission.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 use Zizaco\Entrust\EntrustPermission;
 
 class Permission extends EntrustPermission
--- app/Role.php
+++ app/Role.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
