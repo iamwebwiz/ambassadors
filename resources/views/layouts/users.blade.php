364e8f5dd2eaf7fe43c761d7abbc7893c596203d
iamwebwiz <iamwebwiz@gmail.com>
2017-12-30 08:47:09 +0100

    layout modified

--- resources/views/layouts/users.blade.php
+++ resources/views/layouts/users.blade.php
@@ -123,18 +123,8 @@
 
         <footer class="footer">
             <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
                 <div class="copyright pull-right">
-                    &copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by <a href="http://www.creative-tim.com">Creative Tim</a>
+                    &copy; 2018 <a href="http://dgambassadors.com">Digital Ambassadors</a> | All Rights Reserved
                 </div>
             </div>
         </footer>
