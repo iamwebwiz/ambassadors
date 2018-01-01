a4f4fbea916defd248bc89090edb7a17737f65fe
iamwebwiz <iamwebwiz@gmail.com>
2018-01-01 17:41:04 +0100

    Boilerplate table added for listing publications

--- resources/views/publisher/publications/index.blade.php
+++ resources/views/publisher/publications/index.blade.php
@@ -4,8 +4,30 @@
 
     <div class="row">
         <div class="col-md-12">
+            <h1>My Publications</h1>
             <div class="card">
-                <div class="content">This is nuts</div>
+                <div class="content">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <th>#</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Client</th>
+                                <th>Date Added</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Advert for CowryWise</td>
+                                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
+                                    <td>CowryWise</td>
+                                    <td>January 1st, 2018</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
