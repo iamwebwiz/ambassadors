751a18b3fa7983913ef3ea5bfe132d6c2ed9d866
iamwebwiz <iamwebwiz@gmail.com>
2018-01-23 04:45:04 +0100

    modification

--- resources/views/layouts/templates/nav.blade.php
+++ resources/views/layouts/templates/nav.blade.php
@@ -127,7 +127,7 @@
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
-                                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
 
                                 <p>
                                     Alexander Pierce - Web Developer
@@ -155,7 +155,7 @@
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                    <a href="{{ url('logout') }}" class="btn btn-default btn-flat">Sign out</a>
                                 </div>
                             </li>
                         </ul>
