97dd866c2c90fd9d4bdd82ef6025d577fb67bff4
iamwebwiz <iamwebwiz@gmail.com>
2018-02-10 15:21:07 +0100

    view for displaying reports modified

--- resources/views/publisher/reports/index.blade.php
+++ resources/views/publisher/reports/index.blade.php
@@ -18,7 +18,28 @@
                         </div>
                     </div>
                     <hr>
-                    {{--  --}}
+                    @if (count($reports) < 1)
+                        <h3 class="text-info text-center">No report for this publication yet.</h3>
+                    @else
+                        <table class="table table-hover">
+                            <thead id="tableHeading">
+                                <th>ID</th>
+                                <th>Screenshot</th>
+                                <th>Date Added</th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                @foreach ($reports as $report)
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
